9353f0732b0feab20c60544b5592d1173396c8db
Henrique <henryqueoli@hotmail.com>
2023-05-17 15:43:55 -0300

    feat: add VInput

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
-import Logo from './assets/logo.svg?raw'
+import { ref } from 'vue';
+import VInput from './components/VInput.vue';
+
+const data = ref({
+  name: '',
+});
+
 </script>
 <template>
-  <div class="h-screen w-screen flex items-center justify-center bg-gray-900 text-white" >
-    <div class="text-center">
-      <div v-html="Logo" class="w-32 h-32 mx-auto" />
-
-      <div class="text-xl">
-        Sidekick-coder template
-      </div>
+  <div class="h-screen w-screen flex items-center justify-center bg-gray-900" >
+    <div class="bg-white p-5">
+      <v-input id="name" label="Name" v-model="data.name" />
 
     </div>
   </div>
